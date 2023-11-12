a871946f7692c4a32c345eb6377ac0470493de61
MartsinovichDanya <email@email.e>
2023-11-12 19:21:27 +0300

    add handlers example

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import asyncio
 import logging
 from os import getenv
 
-# from handlers.echo import echo_router
-# from handlers.start import start_router
+from handlers.echo import echo_router
+from handlers.start import start_router
 
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
@@ -14,10 +14,10 @@ TOKEN = getenv("BOT_TOKEN")
 
 async def main() -> None:
     dp = Dispatcher()
-    # dp.include_routers(
-    #     start_router,
-    #     echo_router,
-    # )
+    dp.include_routers(
+        start_router,
+        echo_router,
+    )
 
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
     await dp.start_polling(bot)
