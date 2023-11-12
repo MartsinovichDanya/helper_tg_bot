186ba811b9a4974691614d89778d1974545deca1
MartsinovichDanya <email@email.e>
2023-11-12 19:05:47 +0300

    add requirements and main

---
+++ main.py
@@ -0,0 +1,28 @@
+import asyncio
+import logging
+from os import getenv
+
+# from handlers.echo import echo_router
+# from handlers.start import start_router
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+
+
+TOKEN = getenv("BOT_TOKEN")
+
+
+async def main() -> None:
+    dp = Dispatcher()
+    # dp.include_routers(
+    #     start_router,
+    #     echo_router,
+    # )
+
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
